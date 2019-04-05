b98a63b0734bce63a19fbf3cadc2f951a1678be5
Matt Crowder <mcrowder65@gmail.com>
2019-04-05 17:21:54 -0400

    done!

--- src/use-local-storage-set-state.js
+++ src/use-local-storage-set-state.js
@@ -1,5 +1,20 @@
+// use-local-storage-set-state.js
+import React from "react";
+import store from "store";
+
 export const useLocalStorageSetState = (initialValue, name) => {
   if (!name) {
     throw new Error("Name must be provided to persist to localStorage");
   }
+  const actualInitialValue =
+    store.get(name) !== undefined ? store.get(name) : initialValue;
+  const [value, setValue] = React.useState(actualInitialValue);
+
+  React.useEffect(
+    () => {
+      store.set(name, value);
+    },
+    [value]
+  );
+  return [value, setValue];
 };
--- src/use-local-storage-set-state.test.js
+++ src/use-local-storage-set-state.test.js
@@ -1,7 +1,29 @@
 // use-local-storage-set-state.test.js
 import React from "react";
+import { render, fireEvent } from "react-testing-library";
 import { useLocalStorageSetState } from "./use-local-storage-set-state";
 
 test("throws when name is not provided", () => {
   expect(() => useLocalStorageSetState(0)).toThrow();
+});
+
+test("persists on component unmounts and rerenders", () => {
+  function Comp() {
+    const [value, setValue] = useLocalStorageSetState(0, "value");
+
+    return (
+      <div>
+        {value}
+        <button onClick={() => setValue(value + 1)}>Add value</button>
+      </div>
+    );
+  }
+  const { getByText, rerender, unmount } = render(<Comp />);
+  expect(getByText(/0/i)).toBeInTheDocument();
+  fireEvent.click(getByText(/add value/i));
+  expect(getByText(/1/i)).toBeInTheDocument();
+
+  unmount();
+  rerender(<Comp />);
+  expect(getByText(/1/i)).toBeInTheDocument();
 });
