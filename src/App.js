b34121f1f588afba0992991dc73b0a6dbdb0a70d
Matt Crowder <mcrowder65@gmail.com>
2019-04-05 17:23:54 -0400

    example

--- src/App.js
+++ src/App.js
@@ -1,28 +1,15 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React from "react";
+import { useLocalStorageSetState } from "./use-local-storage-set-state";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+function App() {
+  const [value, setValue] = useLocalStorageSetState(0, "value");
+
+  return (
+    <div>
+      {value}
+      <button onClick={() => setValue(value + 1)}>Increment</button>
+    </div>
+  );
 }
 
 export default App;
